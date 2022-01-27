38ef1973e95501d8fe7fc316080a993c5a35937a
Youssef Boubekri <youssef.boubekri@gmail.com>
2022-01-27 11:50:20 +0100

    recipe model structure

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	Name          string    `json:"Name"`
+	Tags          []string  `json:"Tags"`
+	Ingredients   []string  `json:"Ingredients"`
+	Instructions  []string  `json:"Instructions"`
+	DatePublished time.Time `json:"DatePublished"`
+}
 
 func main() {
 	router := gin.Default()
